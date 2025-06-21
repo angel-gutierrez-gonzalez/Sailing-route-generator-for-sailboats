ad91a73367f421a6c1e13a721b0a7d640f5dca35
Angel Gutiérrez <aigutigon@gmail.com>
2025-06-21 11:52:33 +0200

    Modulo de imitación para DRL aprenda del agente determinista

---
+++ tfm_drl_agent/training/imitation_learning.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import tensorflow as tf
+
+def load_expert_data(csv_path):
+    df = pd.read_csv(csv_path)
+    X = df[["latitude", "longitude", "heading", "speed", "wind_dir", "wind_speed"]].values
+    y = df[["target_heading", "target_speed"]].values
+    return X, y
+
+def train_actor_supervised(actor_model, data_path, epochs=50, batch_size=64, learning_rate=1e-4):
+    X, y = load_expert_data(data_path)
+    dataset = tf.data.Dataset.from_tensor_slices((X, y)).batch(batch_size).shuffle(1000)
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate)
+    loss_fn = tf.keras.losses.MeanSquaredError()
+
+    for epoch in range(epochs):
+        epoch_loss = 0
+        for step, (xb, yb) in enumerate(dataset):
+            with tf.GradientTape() as tape:
+                pred = actor_model(xb, training=True)
+                loss = loss_fn(yb, pred)
+            grads = tape.gradient(loss, actor_model.trainable_variables)
+            optimizer.apply_gradients(zip(grads, actor_model.trainable_variables))
+            epoch_loss += loss.numpy()
+
+        avg_loss = epoch_loss / (step + 1)
+        print(f"Epoch {epoch+1}/{epochs} - Loss: {avg_loss:.4f}")
+
+    return actor_model
