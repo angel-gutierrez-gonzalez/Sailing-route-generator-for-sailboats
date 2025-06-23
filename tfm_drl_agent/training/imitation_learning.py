ffe06259cecfd2b8a192f3ab3898c312e887005e
Angel Gutiérrez <aigutigon@gmail.com>
2025-06-23 12:25:13 +0200

    Modificación de Imitation_learning para la carga de multiples rutas en ficheros CSV

--- tfm_drl_agent/training/imitation_learning.py
+++ tfm_drl_agent/training/imitation_learning.py
@@ -1,14 +1,25 @@
 import pandas as pd
 import tensorflow as tf
+import os
+import glob
 
-def load_expert_data(csv_path):
-    df = pd.read_csv(csv_path)
-    X = df[["latitude", "longitude", "heading", "speed", "wind_dir", "wind_speed"]].values
-    y = df[["target_heading", "target_speed"]].values
-    return X, y
+def load_expert_data_multiple(folder_path):
+    all_files = glob.glob(os.path.join(folder_path, "*.csv"))
+    X_list, y_list = [], []
 
-def train_actor_supervised(actor_model, data_path, epochs=50, batch_size=64, learning_rate=1e-4):
-    X, y = load_expert_data(data_path)
+    for file in all_files:
+        df = pd.read_csv(file)
+        X = df[["latitude", "longitude", "heading", "speed", "wind_dir", "wind_speed"]].values
+        y = df[["target_heading", "target_speed"]].values
+        X_list.append(X)
+        y_list.append(y)
+
+    X_all = pd.concat([pd.DataFrame(x) for x in X_list], ignore_index=True).values
+    y_all = pd.concat([pd.DataFrame(y) for y in y_list], ignore_index=True).values
+    return X_all, y_all
+
+def train_actor_supervised(actor_model, folder_path, epochs=50, batch_size=64, learning_rate=1e-4):
+    X, y = load_expert_data_multiple(folder_path)
     dataset = tf.data.Dataset.from_tensor_slices((X, y)).batch(batch_size).shuffle(1000)
 
     optimizer = tf.keras.optimizers.Adam(learning_rate)
