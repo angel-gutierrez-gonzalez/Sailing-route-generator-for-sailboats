dc323dcef47f68a857e6f3030e0adb240a208028
angel-gutierrez-gonzalez <angel.gutierrez.gonzalez@alumnos.upm.es>
2025-07-03 11:11:54 +0000

    Modificación ruta

--- tfm_deterministic_agent/utils/config.py
+++ tfm_deterministic_agent/utils/config.py
@@ -6,5 +6,5 @@ API_KEY_SG = "b73c5ca4-3e4e-11f0-976d-0242ac130006-b73c5d4e-3e4e-11f0-976d-0242a
 USER_COPER =  "aigutigon@gmail.com"
 PASSWORD_COPER = "ThsXf_eJ*_rf8Ad"
 
-BASE_DIR = os.path.abspath(os.path.join(os.getcwd(), '..'))
+BASE_DIR = os.path.abspath(os.path.join(os.getcwd(), '../..'))
 SAVE_DIR = os.path.join(BASE_DIR, 'data', 'raw')
