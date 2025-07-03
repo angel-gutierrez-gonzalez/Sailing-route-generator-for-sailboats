65d1d8f15145698560a9e88f5dddbe6d5b2aea7a
angel-gutierrez-gonzalez <angel.gutierrez.gonzalez@alumnos.upm.es>
2025-07-03 11:16:35 +0000

    Seleccion de fichero de datos climatológico e inicio y fin de ruta de manera aleatoria entre posibilidades

--- tfm_drl_agent/environment/random_start_goal_wrapper.py
+++ tfm_drl_agent/environment/random_start_goal_wrapper.py
@@ -1,8 +1,8 @@
 import random
-import pandas as pd
-import gym
+from utils.utils import select_random_start_goal, haversine
 
-class RandomizedWindWrapper(gym.Env):
+
+class RandomStartGoalWrapper:
     def __init__(self, env_class, config_base, wind_field):
         self.env_class = env_class
         self.config_base = config_base
@@ -14,9 +14,23 @@ class RandomizedWindWrapper(gym.Env):
 
     def _reset_env(self):
         self.selected_date = random.choice(self.available_dates)
+        df_day = self.wind_field.wind_data[self.selected_date]
+
+        # Seleccionar start y goal válidos para ese día
+        start, goal = select_random_start_goal(df_day)
+        
+        dist_km = haversine(start[1], start[0], goal[1], goal[0])
+        print(f"[RUTA] Día: {self.selected_date} | Inicio: {start} | Destino: {goal} | Distancia: {dist_km:.2f} km")
+
+
+        # Construir config
         config = self.config_base.copy()
-        config['wind'] = self.wind_field
+        config['start'] = start
+        config['goal'] = goal
+        config['wind'] = self.wind_field.get_day(self.selected_date)
         config['wind_date'] = self.selected_date
+
+        # Construir entorno interno
         self.env = self.env_class(config)
 
     def reset(self):
@@ -29,17 +43,5 @@ class RandomizedWindWrapper(gym.Env):
     def render(self, mode='human'):
         return self.env.render(mode)
 
-    def close(self):
-        return self.env.close()
-
-    @property
-    def action_space(self):
-        return self.env.action_space
-
-    @property
-    def observation_space(self):
-        return self.env.observation_space
-
-    @property
-    def current_wind_df(self):
-        return self.wind_field.wind_data[self.selected_date]
+    def __getattr__(self, name):
+        return getattr(self.env, name)
